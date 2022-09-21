5a247aae8308990ca9caf9d8452ad6cc0abf416f
NascimentoFrancisco <fmilk9049@gmail.com>
2022-09-21 15:49:04 -0300

    Fazendo rotas de erros

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from .views import HomeView
+from .views import HomeView, erro_view_404, erro_view_500
+
+from django.urls import re_path as url
+from django.conf import settings
+from django.views.static import serve
+
+handler404 = erro_view_404
+handler500 = erro_view_500
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('accounts/',include('accounts.urls',namespace='accounts')),
--- setup/views.py
+++ setup/views.py
@@ -5,3 +5,9 @@ class HomeView(View):
     
     def get(self, request):
         return render(request, 'home.html')
+
+def erro_view_404(request, *args, **kwargs):
+    return render(request,'pages_erros/erro_404.html')
+
+def erro_view_500(request, *args, **kwargs):
+    return render(request,'pages_erros/erro_500.html')
